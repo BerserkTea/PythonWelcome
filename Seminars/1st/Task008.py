2e1d80c6b8bb18dd134f48fd664c54f70b9bb8a7
«AndronovIvan» <andronoviv4@gmail.com>
2022-03-05 19:28:45 +0300

    add solution Task008

---
+++ Seminars/1st/Task008.py
@@ -0,0 +1,15 @@
+#Сообщить в какой четверти координатной 
+# плоскости или на какой оси находится 
+# точка с координатами Х и У 
+
+coordinateX = int(input('Введите координату х '))
+coordinateY = int(input('Введите координату y '))
+
+if coordinateX > 0 and coordinateY>0:
+    print("Точка находится в 1 четверти")
+if coordinateX < 0 and coordinateY>0:
+    print("Точка находится в 2 четверти")
+if coordinateX < 0 and coordinateY<0:
+    print("Точка находится в 3 четверти")
+if coordinateX > 0 and coordinateY<0:
+    print("Точка находится в 4 четверти")
