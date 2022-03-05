5e2aa757f9afd80400a8da844229e14c4446c8ee
«AndronovIvan» <andronoviv4@gmail.com>
2022-03-05 21:10:27 +0300

    add 2nd solution Task013

---
+++ Seminars/1st/Task013.py
@@ -0,0 +1,11 @@
+# Пользователь задаёт две строки. Определить количество вхождений одной строки в другой.
+str1 = "Мультики» — году году году роман Михаила Елизарова, изданный в 2010 году издательством АСТ."
+str2 = "году"
+def findHowMuch (a,b):
+    counter = 0
+    for i in a.split():
+        # print(str(i))
+        if i.find(b) != -1:
+            counter+=1
+    return counter
+print (findHowMuch(str1,str2))
