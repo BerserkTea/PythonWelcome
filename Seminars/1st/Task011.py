a7d2a738a24e2cc94e1950e4cb214eeb5d3b6563
«AndronovIvan» <andronoviv4@gmail.com>
2022-03-05 20:19:56 +0300

    add 2nd solution Task011

--- Seminars/1st/Task011.py
+++ Seminars/1st/Task011.py
@@ -1,6 +1,15 @@
 # Сформировать список из  N членов последовательности.
+# Для N = 5: 1, -3, 9, -27, 81 и т.д.
 N = int(input('Введите N'))
+numbers = list(range(0, N))
+print(numbers)
+for i in numbers:
+    numbers[i] = 3**i
+    if i%2!=0:
+        numbers[i] *= -1
 
-generator = range(0, N+1)
+print(numbers)
 
-print(list(generator))
+
+numbers2 = [3**i for i in range (0,N)]
+print(numbers2)
