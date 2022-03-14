c3691877946f11328a90fa5613ac25eb8d852a7b
«AndronovIvan» <andronoviv4@gmail.com>
2022-03-14 10:56:06 +0300

    its ok

---
+++ Seminars/1st/Task014.py
@@ -0,0 +1,11 @@
+# Подсчитать сумму цифр в вещественном числе.
+
+number = 25215.123123
+# def summaryDigits(somenumber):
+#     summ=0
+#     spisokList = str(somenumber).split(".")
+#     for i in spisokList:
+#         list
+#     return summ
+print(summaryDigits(number))
+sum(map)
